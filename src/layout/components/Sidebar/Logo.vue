c71c707660998b83c5713a6a18e42a534dfea7cc
Write λ Code <story0809@163.com>
2024-11-14 17:34:28 +0800

    修复集成 tailwind logo不展示的问题

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sidebar-logo-container" :class="{ 'collapse': collapse }" :style="{ backgroundColor: sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground }">
+  <div class="sidebar-logo-container" :class="{ 'xm-collapse': collapse }" :style="{ backgroundColor: sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground }">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo" />
@@ -72,7 +72,7 @@ const sideTheme = computed(() => settingsStore.sideTheme);
     }
   }
 
-  &.collapse {
+  &.xm-collapse {
     .sidebar-logo {
       margin-right: 0px;
     }
